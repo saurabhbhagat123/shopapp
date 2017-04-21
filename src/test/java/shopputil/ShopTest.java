33628d548db5f0a3708ac5ccd1d5f8fd8bec6656
unknown <saurabhbhagat01@gmail.com>
2017-04-20 23:04:49 -0700

    New changes

--- src/test/java/shopputil/ShopTest.java
+++ src/test/java/shopputil/ShopTest.java
@@ -6,29 +6,31 @@ import java.util.List;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.springframework.boot.test.SpringApplicationConfiguration;
-import org.springframework.boot.test.WebIntegrationTest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpEntity;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.RestTemplate;
 
 import com.shop.MainApplication;
 import com.shop.domain.Shop;
 import com.shop.response.NearestShopResponse;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringApplicationConfiguration(classes = MainApplication.class)
-@WebIntegrationTest
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes=MainApplication.class,webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+
 public class ShopTest {
 
 
+	@Autowired
+	private TestRestTemplate restTemplate;
 	
 	public void testAddShops(){
 
-		RestTemplate restTemplate=new RestTemplate();
 		String json="{\"shops\": [{\"shopname\": \"Destination Centre\",\"address\": {\"streetname\": \"Magarpatta City,Hadapsar\",\"city\": \"Pune\",\"state\": \"Maharashtra\",\"postcode\": 411038}}]}";
 		HttpHeaders requestHeaders = new HttpHeaders();
 		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
@@ -40,13 +42,12 @@ public class ShopTest {
 
 	@Test
 	public void testGetNearestShop(){
-		RestTemplate restTemplate=new RestTemplate();
 		String json="{\"shops\": [{\"shopname\": \"Destination Centre\",\"address\": {\"streetname\": \"Magarpatta City,Hadapsar\",\"city\": \"Pune\",\"state\": \"Maharashtra\",\"postcode\": 411028}},{\"shopname\": \"Seasons Mall\",\"address\": {\"streetname\": \"Magarpatta City,Hadapsar\",\"city\": \"Pune\",\"state\": \"Maharashtra\",\"postcode\": 411013}}]}";
 		HttpHeaders requestHeaders = new HttpHeaders();
 		requestHeaders.setContentType(MediaType.APPLICATION_JSON);
 		HttpEntity<String> httpEntity =new HttpEntity<String>(json, requestHeaders);
-		restTemplate.postForEntity("http://localhost:8080/shop-app/shops", httpEntity,new ArrayList<Shop>().getClass());
-		ResponseEntity<NearestShopResponse> responseEntity = restTemplate.getForEntity("http://localhost:8080/shop-app/shop?lat=18.526792&long=73.9071402", NearestShopResponse.class);
+		restTemplate.postForEntity("/shops", httpEntity,new ArrayList<Shop>().getClass());
+		ResponseEntity<NearestShopResponse> responseEntity = restTemplate.getForEntity("/shop?lat=18.526792&long=73.9071402", NearestShopResponse.class);
 		NearestShopResponse nearestShop=responseEntity.getBody();
 		Assert.assertEquals("Destination Centre", nearestShop.getNearestShop());
 		
